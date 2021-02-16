95e465aa0d443e6075a4b7fff4127c7e20aec381
Michal <mg.michbisz.mg@gmail.com>
2021-02-16 19:44:21 +0100

    menu

--- js/hooks.js
+++ js/hooks.js
@@ -3,13 +3,12 @@ import { useEffect } from 'react';
 export const useOnClickOutside = (ref, handler) => {
     useEffect(() => {
             const listener = event => {
-                if (ref.current || !ref.current.contains(event.target) && !ref.current) {
+                if (!ref.current || !ref.current.contains(event.target)) {
                     return;
                 }
                 handler(event);
             };
             document.addEventListener('mousedown', listener);
-
             return () => {
                 document.removeEventListener('mousedown', listener);
             };
